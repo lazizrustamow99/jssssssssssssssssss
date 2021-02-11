6563fe04df48990e95881d0ac0e325b94fc99a1b
lazizrustamow99 <77507970+lazizrustamow99@users.noreply.github.com>
2021-02-11 18:20:25 +0500

    Add files via upload

---
+++ 1/script.js
@@ -0,0 +1,11 @@
+for (let i = 1; i < 10; i++) {
+    var userName = prompt('Введите своё имя!');
+    var userAge = +prompt('Введите свой возраст!');
+
+    const objUser = {
+        name: userName,
+        age: userAge
+              
+    };
+    console.log(`Пользователь: ${userName}, Возраст: ${userAge}`);
+}
